4ec4ff44c06ec9fa1a3a7babdaa004a1b64b8c87
Long Huynh <longbhuynh019@gmail.com>
2022-01-08 01:39:02 -0500

    When typing in an entry,it goes to that page with info abt that entry

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclo"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.content, name="page")
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
 from markdown2 import Markdown
+from django import forms
 
 markdowner = Markdown()
 
+class newEncycloForm(forms.Form):
+    entry = forms.CharField(label = "Search Encyclopedia")
 
 def index(request):
+    if request.method == "POST":
+        form = newEncycloForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["entry"]
+            return HttpResponseRedirect(reverse("encyclo:page",  args=(entry,)))
+        else:
+            return form.errors
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": newEncycloForm()
     })
 def content(request, title):
     page = util.get_entry(title)
     return render(request, "encyclopedia/entries.html", {
+
         "title": title.capitalize(),
         "entry": markdowner.convert(page)
     })
