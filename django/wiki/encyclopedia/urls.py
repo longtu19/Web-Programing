e7b7328976a8d7a8841f99e63f536317f055f1a9
Long Huynh <longbhuynh019@gmail.com>
2022-01-07 20:46:54 -0500

    second commit

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.content, name="entry")
+    path("<str:title>", views.content, name="page")
 ]
