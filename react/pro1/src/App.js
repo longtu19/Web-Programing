e44f42b8399f7f4823034fd5277da1c8c930291f
Long Huynh <longbhuynh019@gmail.com>
2022-03-12 21:56:41 -0500

    collapsed demo

--- react/pro1/src/App.js
+++ react/pro1/src/App.js
@@ -1,32 +1,90 @@
 import './App.css';
 import Info from './Info.js';
 import { useState } from "react";
+import useCollapse from 'react-collapsed';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Info />
       <ButtonState />
     </div>
   );
 }
 
 function ButtonState(){
-  const [title, setTitle] = useState("");
-  const [count, setCount] = useState(0);
-  const updateTitle = () => {
-    setTitle("We have a title!");
-  }
-  const updateCount = () => {
-    setCount(count + 1);
-  }
+
+  
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+  return (
+    <div className="collapsible">
+      <ul>Home</ul>
+      <ul>Service</ul>
+        <button className="header" {...getToggleProps()}>
+            {isExpanded ? 'Hide' : 'Expand'}
+        </button>
+        <div {...getCollapseProps()}>
+            <div className="content">
+                Cooking <br/><br/>
+                Cleaning
+              
+            </div>
+        </div>
+
+        <ul>Contact</ul>
+        <button className="header" {...getToggleProps()}>
+            {isExpanded ? 'Hide' : 'Expand'}
+        </button>
+        <div {...getCollapseProps()}>
+            <div className="content">
+                Phone <br/><br/>
+                Mailing
+              
+            </div>
+        </div>
+    </div>
+  );  
+  
+  // const updateSer = () => {
+  //   <div>
+  //     <SerState />
+
+  //   </div>
+    
+  // }
+  // const updateCon = () => {
+  //   <div>
+  //     <ContState />
+
+  //   </div>
+    
+  // }
+  // return (
+  //   <div>
+  //     <ul>Home</ul>
+  //     <ul>Service</ul>
+  //     <button onClick = {updateSer} >Expand</button>
+  //     <ul>Contact</ul>
+  //     <button onClick = {updateCon} >Expand</button>
+  //   </div>
+
+  // )
+}
+function SerState(){
   return (
     <div>
-      <Data title = {title} count = {count} />
-      <button onClick = {updateTitle} >Update Title</button>
-      <button onClick = {updateCount} >Update Counter</button>
+      <ul>Cooking</ul>
+      <ul>Cleaning</ul>
     </div>
-
+  )
+}
+function ContState(){
+  return (
+    <div>
+      <ul>Phone</ul>
+      <ul>Mailing</ul>
+    </div>
   )
 }
 
