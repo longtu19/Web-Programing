9ee3538427cb92ed77ae44fe28dc4bf5aa668ea6
Long Huynh <longbhuynh019@gmail.com>
2022-03-25 23:03:01 -0400

    UI updated

--- react/pro1/src/App.js
+++ react/pro1/src/App.js
@@ -1,101 +1,89 @@
+import { useState } from 'react';
 import './App.css';
-import Info from './Info.js';
-import { useState } from "react";
-import useCollapse from 'react-collapsed';
+import SearchBar from './SearchBar'
+import AddItem from './AddItem'
+import ItemsDisplay from './ItemsDisplay'
+import { responsivePropType } from 'react-bootstrap/esm/createUtilityClasses';
+
 
 
 
 function App() {
+
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({items: [] })
+
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  }
+
+  const addItemToData = (item) => {
+    let items = data['items']
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+
+      },
+      body: JSON.stringify(item),
+    }
+    fetch("http://localhost:3000/items", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      items.push(data)
+      setData({ items: items })
+
+    })
+
+    
+
+  }
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name){
+      return data;
+    }
+
+    for (const item of data){
+      if (filters.name !== "" && item.name !== filters.name){
+         continue
+      }
+       if (filters.price !== 0 && item.price > filters.price){ 
+         continue
+      }
+      if (filters.type !== "" && item.type !== filters.type){ 
+         continue
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand){ 
+         continue
+      }
+
+      filteredData.push(item)
+
+    }
+
+    return filteredData
+  }
+
   return (
-    <div className="App">
-      <ButtonState />
+    <div className="container">
+      <div className = "row mt-3"><ItemsDisplay items = {filterData(data['items'])} /></div>
+      <div className = "row mt-3"><SearchBar updateSearch = {updateFilters} /></div>
+      
+      <div className = "row mt-3"><AddItem addItem = {addItemToData}/></div>
+
+      
     </div>
+    
   );
 }
 
-function ButtonState(){
 
-  
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
-  return (
-    <div className="collapsible">
-      <ul>Home</ul>
-      <ul>Service</ul>
-        <button className="header" {...getToggleProps()}>
-            {isExpanded ? 'Hide' : 'Expand'}
-        </button>
-        <div {...getCollapseProps()}>
-            <div className="content">
-                Cooking <br/><br/>
-                Cleaning
-              
-            </div>
-        </div>
 
-        <ul>Contact</ul>
-        <button className="header" {...getToggleProps()}>
-            {isExpanded ? 'Hide' : 'Expand'}
-        </button>
-        <div {...getCollapseProps()}>
-            <div className="content">
-                Phone <br/><br/>
-                Mailing
-              
-            </div>
-        </div>
-    </div>
-  );  
-  
-  // const updateSer = () => {
-  //   <div>
-  //     <SerState />
-
-  //   </div>
-    
-  // }
-  // const updateCon = () => {
-  //   <div>
-  //     <ContState />
-
-  //   </div>
-    
-  // }
-  // return (
-  //   <div>
-  //     <ul>Home</ul>
-  //     <ul>Service</ul>
-  //     <button onClick = {updateSer} >Expand</button>
-  //     <ul>Contact</ul>
-  //     <button onClick = {updateCon} >Expand</button>
-  //   </div>
-
-  // )
-}
-function SerState(){
-  return (
-    <div>
-      <ul>Cooking</ul>
-      <ul>Cleaning</ul>
-    </div>
-  )
-}
-function ContState(){
-  return (
-    <div>
-      <ul>Phone</ul>
-      <ul>Mailing</ul>
-    </div>
-  )
-}
-
-function Data(props){
-  return (
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Count: {props.count} </p>
-    </div>
-  )
-}
 
 
 export default App;
