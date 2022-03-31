cc79604bc5b608f92d066ee3baadfbb92c486a29
Long Huynh <longbhuynh019@gmail.com>
2022-03-31 03:57:08 -0400

    day 2

--- react/restaurant/src/App.js
+++ react/restaurant/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import React, { Component } from 'react'
 import Main from './components/MainComponent'
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>  
+          <Main />
+        </div>
+      
+      </BrowserRouter>
+      
     )
   }
  
--- react/restaurant/src/components/MenuComponents.js
+++ react/restaurant/src/components/MenuComponents.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
  
     function RenderMenuItem({dish, onClick}){
         return (
-            <Card onClick = {() => onClick(dish.id)}>
-                    <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
+            <Card >
+
+                <Link to ={`/menu/${dish.id}`} />
+                <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
             </Card>
 
         )
@@ -18,7 +21,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
         const menu =  props.dishes.map((dish) => {
             return (
                 <div key = {dish.id} className = "col-12 col-md-5 m-1">
-                    <RenderMenuItem dish = {dish} onClick = {props.onClick} />
+                    <RenderMenuItem dish = {dish} />
                     
                 </div>
             )
