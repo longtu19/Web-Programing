7cb5f6b30fdd52070cdf620e8377c5c451778fb8
Long Huynh <longbhuynh019@gmail.com>
2022-03-31 21:12:16 -0400

    single page app

--- react/restaurant/src/components/MenuComponents.js
+++ react/restaurant/src/components/MenuComponents.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
  
@@ -7,11 +7,14 @@ import { Link } from 'react-router-dom'
         return (
             <Card >
 
-                <Link to ={`/menu/${dish.id}`} />
-                <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                <Link to ={`/menu/${dish.id}`}>
+                    <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+
+                </Link>
+                
             </Card>
 
         )
@@ -29,6 +32,19 @@ import { Link } from 'react-router-dom'
 
         return (
             <div className = "container">
+                <div className = "row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to = '/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className = "col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+
+                    </div>
+                </div>
                 <div className = "row">
                     {menu}
                 </div> 
