749197e9cc60c25fa35f14696f29ddcf206a4f03
Long Huynh <longbhuynh019@gmail.com>
2022-04-02 23:16:24 -0400

    applied Redux

--- react/restaurant/src/App.js
+++ react/restaurant/src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 import React, { Component } from 'react'
 import Main from './components/MainComponent'
-import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from './Redux/configureStore'
+
+const store = configureStore()
 
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>  
-          <Main />
-        </div>
+
+      <Provider store = {store}>
+          <BrowserRouter>
+          <div>  
+            <Main />
+          </div>
+        
+        </BrowserRouter>
+
+      </Provider>
       
-      </BrowserRouter>
       
     )
   }
